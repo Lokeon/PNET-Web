54017a8caf83a76d4f4bd2b3b9c2a9b4ff8ccab0
Kevin López <kevin.lopezcala99@gmail.com>
2020-03-24 03:55:48 +0100

    :penguin:

--- js/tabs.js
+++ js/tabs.js
@@ -1,16 +1,18 @@
-var tabButtons=document.querySelectorAll(".tabContainer .buttonContainer button");
-var tabPanels=document.querySelectorAll(".tabContainer  .tabPanel");
+var tabButtons = document.querySelectorAll(
+  ".tabContainer .buttonContainer button"
+);
+var tabPanels = document.querySelectorAll(".tabContainer  .tabPanel");
 
-function showPanel(panelIndex,colorCode) {
-    tabButtons.forEach(function(node){
-        node.style.backgroundColor="";
-        node.style.color="";
-    });
-    tabButtons[panelIndex].style.backgroundColor=colorCode;
-    tabButtons[panelIndex].style.color="black";
-    tabPanels.forEach(function(node){
-        node.style.display="none";
-    });
-    tabPanels[panelIndex].style.display="block";
+function showPanel(panelIndex, colorCode) {
+  tabButtons.forEach(function(node) {
+    node.style.backgroundColor = "";
+    node.style.color = "";
+  });
+  tabButtons[panelIndex].style.backgroundColor = colorCode;
+  tabButtons[panelIndex].style.color = "black";
+  tabPanels.forEach(function(node) {
+    node.style.display = "none";
+  });
+  tabPanels[panelIndex].style.display = "block";
 }
-showPanel(0,'#D8AC34');
+showPanel(0, "#D8AC34");
